bc7454b3da6a7f37ad3622f8beb0ce93633b95ec
Jahnavi Gulati <48859061+jahnavi0301@users.noreply.github.com>
2021-03-02 06:37:49 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+  
+<?php
+if(!session_id()) session_start();
+$filename = 0;
+if(!isset($_SESSION['filename'])) {
+    $_SESSION['filename'] = $filename;
+}
+
+header('location:frontPage.php');
+
+?>
